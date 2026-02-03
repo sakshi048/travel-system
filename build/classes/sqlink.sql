8fef56ea17908a1249f99c2eb2895ce2a7eaf454
Sakshi Gharat <sakshigharat932@gmail.com>
2026-02-03 23:42:31 +0530

    Clean up repository: remove IDE and build files

--- build/classes/sqlink.sql
+++
@@ -1,9 +0,0 @@
-create database traveelmanagementsystem;
-
-show databases;
-
-use travelmanagementsystem;
-
-create table account(username varchar(20),name varchar(20),password varchar(20),security varchar(20),answer varchar(20));
-
-show tables;
--- build/classes/travel/management/system/ForgotPass.rs
+++
@@ -1,6 +0,0 @@
-ForgotPass$LoadingPage
-ForgotPass$2
-ForgotPass$1
-ForgotPass$LoadingPage$1
-ForgotPass$ProgressBar
-ForgotPass
